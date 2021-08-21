b83916b563a31ec0c11732aba19501b558c802d6
John Redden <jredden@Johns-MacBook-Air.local>
2021-08-21 11:34:58 -0400

    added basic board

---
+++ chessboard/src/components/Space.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Space.css";
+
+const Space = () => {
+  return (
+    <div>
+      <div className="space"></div>
+    </div>
+  );
+};
+
+export default Space;
